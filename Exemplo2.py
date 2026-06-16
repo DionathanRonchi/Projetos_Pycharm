7637b86bbdb76692827bba6e92cfb512dc7ae351
Aluno <dionathanfrancisco17@gmail.com>
2026-06-16 09:14:02 -0300

    Exemplo 2

---
+++ Exemplo2.py
@@ -0,0 +1,5 @@
+#Exemplo 2
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+dinheiro = float(input("Digite o valor do dinheiro: "))
+print(f"Nome {nome} - {idade} Anos - R${dinheiro:.2f}")
